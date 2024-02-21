cbe22a4587c489cfa39afa1fd16e3852792e208d
yasminbia8 <106694247+yasminbia8@users.noreply.github.com>
2024-02-21 14:41:55 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
+const botoes = document.querySelectorAll(".botao");
 
+for(let i=0;i <botoes.length;i++){
+  botoes[i].onclick = function(){
+    
+ for(let j=0;j<botoes.length;j++){
+   botoes[j].classlist.remove("ativo");
+ }
+
+    botoes[i].classlist.add("ativo");
+  }
+}
