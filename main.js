b12a255670ff6df18bf75a13a7f15e84c14f2ac4
yasminbia8 <106694247+yasminbia8@users.noreply.github.com>
2024-03-13 14:33:33 -0300

    Update main.js

--- main.js
+++ main.js
@@ -9,3 +9,21 @@ texto[j].classList.remove("ativo");
     botoes[i].classlist.add("ativo");
   }
 }
+
+
+
+
+
+
+
+
+
+contadores[0].textContentt = calculaTempo(tempoObjetivo);
+
+function calculaTempo(tempoObjetivo) {
+  let tempoAtual = new Date();
+  let tempoFinal = tempoObjetivo - tempoAtual;
+  let segundo = Math.floor(tempoFinal / 1000);
+  let minutos = Math.floor(segundos / 60);
+  let horas = Math.floor(minutos / 60);
+  let dias = Math.floor(horas / 24);
