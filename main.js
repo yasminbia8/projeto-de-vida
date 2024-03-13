f7b67edb26b35e16ed991fe41dc88c696387e65d
yasminbia8 <106694247+yasminbia8@users.noreply.github.com>
2024-03-13 15:28:09 -0300

    Update main.js

--- main.js
+++ main.js
@@ -34,4 +34,9 @@ function calculaTempo(tempoObjetivo) {
   segundos %= 60;%
   minutos %= 60;
   horas %= 24;
+if (tempoFinal > 0){
   return dias + " dias "+ horas + " + minutos " + segundos + " segundos";
+} else {
+ return "Prazo Finalizado";
+   }
+}
